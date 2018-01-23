9e38c3e3a62393ed844ad2b5934606feeea887ab
Marcelo Franco <marcelofranco.ti@gmail.com>
2018-01-23 02:24:54 -0200

    BAnco na unha

--- ProAgenda.Web/Models/DiaryEvents.cs
+++ ProAgenda.Web/Models/DiaryEvents.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Globalization; // << dont forget to add this for converting dates to localtime
 using System.Data.Entity.Core.Objects;
+using ProAgenda.Web.Models;
 
 namespace ProAgenda.Web
 {
@@ -22,34 +23,31 @@ namespace ProAgenda.Web
 
         public static List<DiaryEvent> LoadAllAppointmentsInDateRange(double start, double end)
         {
-             var fromDate = ConvertFromUnixTimestamp(start);
+            var fromDate = ConvertFromUnixTimestamp(start);
             var toDate = ConvertFromUnixTimestamp(end);
-            using (DiaryContainer ent = new DiaryContainer())
+
+            var rslt = TesteBanco.GetToListAgenda();
+       
+
+            List<DiaryEvent> result = new List<DiaryEvent>();
+            foreach (var item in rslt)
             {
-
-
-                var rslt = ent.AppointmentDiaries.Where(s => s.DateTimeScheduled >= fromDate && System.Data.Entity.DbFunctions.AddMinutes(s.DateTimeScheduled, s.AppointmentLength) <= toDate);
-
-
-                List<DiaryEvent> result = new List<DiaryEvent>();
-                foreach (var item in rslt)
-                {
-                    DiaryEvent recs = new DiaryEvent();
-                    recs.ID = item.ID;
-                    recs.SomeImportantKeyID = item.SomeImportantKey;
-                    recs.StartDateString = item.DateTimeScheduled.ToString("s"); // "s" is a preset format that outputs as: "2009-02-27T12:12:22"
-                    recs.EndDateString = item.DateTimeScheduled.AddMinutes(item.AppointmentLength).ToString("s"); // field AppointmentLength is in minutes
-                    recs.Title = item.Title + " - " + item.AppointmentLength.ToString() + " mins";
-                    recs.StatusString = Enums.GetName<AppointmentStatus>((AppointmentStatus)item.StatusENUM);
-                    recs.StatusColor = Enums.GetEnumDescription<AppointmentStatus>(recs.StatusString);
-                    string ColorCode = recs.StatusColor.Substring(0, recs.StatusColor.IndexOf(":"));
-                    recs.ClassName = recs.StatusColor.Substring(recs.StatusColor.IndexOf(":") + 1, recs.StatusColor.Length - ColorCode.Length - 1);
-                    recs.StatusColor = ColorCode;
-                    result.Add(recs);
-                }
-
-                return result;
+                DiaryEvent recs = new DiaryEvent();
+                recs.ID = item.ID;
+                recs.SomeImportantKeyID = item.SomeImportantKey;
+                recs.StartDateString = item.DateTimeScheduled.ToString("s"); // "s" is a preset format that outputs as: "2009-02-27T12:12:22"
+                recs.EndDateString = item.DateTimeScheduled.AddMinutes(item.AppointmentLength).ToString("s"); // field AppointmentLength is in minutes
+                recs.Title = item.Title + " - " + item.AppointmentLength.ToString() + " mins";
+                recs.StatusString = Enums.GetName<AppointmentStatus>((AppointmentStatus)item.StatusENUM);
+                recs.StatusColor = Enums.GetEnumDescription<AppointmentStatus>(recs.StatusString);
+                string ColorCode = recs.StatusColor.Substring(0, recs.StatusColor.IndexOf(":"));
+                recs.ClassName = recs.StatusColor.Substring(recs.StatusColor.IndexOf(":") + 1, recs.StatusColor.Length - ColorCode.Length - 1);
+                recs.StatusColor = ColorCode;
+                result.Add(recs);
             }
+
+            return result;
+
 
         }
 
@@ -59,51 +57,33 @@ namespace ProAgenda.Web
 
             var fromDate = ConvertFromUnixTimestamp(start);
             var toDate = ConvertFromUnixTimestamp(end);
-            using (DiaryContainer ent = new DiaryContainer())
+        
+            var rslt = TesteBanco.GetAgendas(start, end);
+
+            List<DiaryEvent> result = new List<DiaryEvent>();
+            int i = 0;
+            foreach (var item in rslt)
             {
-                var rslt = ent.AppointmentDiaries.Where(s => s.DateTimeScheduled >= fromDate && System.Data.Entity.DbFunctions.AddMinutes(s.DateTimeScheduled, s.AppointmentLength) <= toDate)
-                                                        .GroupBy(s => System.Data.Entity.DbFunctions.TruncateTime(s.DateTimeScheduled))
-                                                        .Select(x => new { DateTimeScheduled = x.Key, Count = x.Count() });
-
-                List<DiaryEvent> result = new List<DiaryEvent>();
-                int i = 0;
-                foreach (var item in rslt)
-                {
-                    DiaryEvent rec = new DiaryEvent();
-                    rec.ID = i; //we dont link this back to anything as its a group summary but the fullcalendar needs unique IDs for each event item (unless its a repeating event)
-                    rec.SomeImportantKeyID = -1;
-                    string StringDate = string.Format("{0:yyyy-MM-dd}", item.DateTimeScheduled);
-                    rec.StartDateString = StringDate + "T00:00:00"; //ISO 8601 format
-                    rec.EndDateString = StringDate + "T23:59:59";
-                    rec.Title = "Booked: " + item.Count.ToString();
-                    result.Add(rec);
-                    i++;
-                }
-
-                return result;
+                DiaryEvent rec = new DiaryEvent();
+                rec.ID = i; //we dont link this back to anything as its a group summary but the fullcalendar needs unique IDs for each event item (unless its a repeating event)
+                rec.SomeImportantKeyID = -1;
+                string StringDate = string.Format("{0:yyyy-MM-dd}", item.DateTimeScheduled);
+                rec.StartDateString = StringDate + "T00:00:00"; //ISO 8601 format
+                rec.EndDateString = StringDate + "T23:59:59";
+                //rec.Title = "Booked: " + item.Count.ToString();
+                result.Add(rec);
+                i++;
             }
+
+            return result;
+
 
         }
 
         public static void UpdateDiaryEvent(int id, string NewEventStart, string NewEventEnd)
         {
             // EventStart comes ISO 8601 format, eg:  "2000-01-10T10:00:00Z" - need to convert to DateTime
-            using (DiaryContainer ent = new DiaryContainer())
-            {
-                var rec = ent.AppointmentDiaries.FirstOrDefault(s => s.ID == id);
-                if (rec != null)
-                {
-                    DateTime DateTimeStart = DateTime.Parse(NewEventStart, null, DateTimeStyles.RoundtripKind).ToLocalTime(); // and convert offset to localtime
-                    rec.DateTimeScheduled = DateTimeStart;
-                    if (!String.IsNullOrEmpty(NewEventEnd))
-                    {
-                        TimeSpan span = DateTime.Parse(NewEventEnd, null, DateTimeStyles.RoundtripKind).ToLocalTime() - DateTimeStart;
-                        rec.AppointmentLength = Convert.ToInt32(span.TotalMinutes);
-                    }
-                    ent.SaveChanges();
-                }
-            }
-
+            TesteBanco.UpdateAgenda(id, NewEventStart, NewEventEnd);
         }
 
 
@@ -113,23 +93,12 @@ namespace ProAgenda.Web
             return origin.AddSeconds(timestamp);
         }
 
+       
+
 
         public static bool CreateNewEvent(string Title, string NewEventDate, string NewEventTime, string NewEventDuration)
         {
-            try
-            {
-                DiaryContainer ent = new DiaryContainer();
-                AppointmentDiary rec = new AppointmentDiary();
-                rec.Title = Title;
-                rec.DateTimeScheduled = DateTime.ParseExact(NewEventDate + " " + NewEventTime, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
-                rec.AppointmentLength = Int32.Parse(NewEventDuration);
-                ent.AppointmentDiaries.Add(rec);
-                ent.SaveChanges();
-            }
-            catch (Exception)
-            {
-                return false;
-            }
+            TesteBanco.CreateNewAgenda(Title, NewEventDate, NewEventTime, NewEventDuration);
             return true;
         }
     }
